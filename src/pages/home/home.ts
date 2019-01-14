d3900c15e65ef1d84b11a8631346eee6931c497f
Mohammed Arqam <mohammedarqam97@gmail.com>
2019-01-14 19:45:37 +0530

    Integrated with extra declarations file

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,42 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  
+
   constructor(public navCtrl: NavController) {
 
   }
+  pay() {
+    var options = {
+      description: 'Credits towards consultation',
+      image: 'https://i.imgur.com/3g7nmJC.png',
+      currency: 'INR',
+      key: 'rzp_test_1DP5mmOlF5G5ag',
+      amount: '5000',
+      name: 'foo',
+      prefill: {
+        email: 'pranav@razorpay.com',
+        contact: '8879524924',
+        name: 'Pranav Gupta'
+      },
+      theme: {
+        color: '#F37254'
+      },
+      modal: {
+        ondismiss: function() {
+          alert('dismissed')
+        }
+      }
+    };
 
+    var successCallback = function(payment_id) {
+      alert('payment_id: ' + payment_id);
+    };
+
+    var cancelCallback = function(error) {
+      alert(error.description + ' (Error ' + error.code + ')');
+    };
+
+    RazorpayCheckout.open(options, successCallback, cancelCallback);
+  }
 }
